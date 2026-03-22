6760a9ea31d233e531d32cadc57048254c728799
bjw4227 <jinwoo4221@gmail.com>
2026-03-22 19:16:51 +0900

    fix: import bug

--- src/constants/time.ts
+++ src/constants/time.ts
@@ -1,4 +1,4 @@
-import { formatTime } from 'utils/time';
+import { formatTime } from 'utils/format';
 
 export const TIME_SLOTS: string[] = Array.from({ length: 23 }, (_, i) =>
   formatTime(Math.floor(i / 2) + 9, (i % 2) * 30)
